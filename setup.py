5017367bf8fadd2de9ef83af2ac62d7871c4920b
rafay183 <rafayshaikh633@gmail.com>
2023-09-12 16:01:58 +0500

    cookiecutter task

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def get_requirements(filepath):
+    requirements = []
+    with open(filepath,'r')as f:
+        requirements = f.readlines()
+        requirements = [r.replace('\n','') for r in requirements]
+    return requirements
+
+
+
+
+setup(
+    name = 'cookiecutter',
+    version = '0.0.1',
+    author = 'rafayshaikh',
+    authoremail = 'rafayshaikh633@gmail.com',
+    packages=find_packages(),
+    install_require = get_requirements('requirements.txt')
+
+
+
+
+)
